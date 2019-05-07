15833748713fd2eae3649ece9424e3391a733e56
benjchand <benchand@gmail.com>
2019-05-07 15:58:20 -0700

    Done with Day 2 MVP.

--- routers/loginRouter.js
+++ routers/loginRouter.js
@@ -17,6 +17,7 @@ router.post("/login/", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        req.session.username = user.username;
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         sendUserError(401, "You shall not pass", res);
--- server.js
+++ server.js
@@ -7,9 +7,23 @@ const registerRouter = require("./routers/registerRouter");
 const authenticationRouter = require("./routers/authenticationRouter");
 const usersRouter = require("./routers/usersRouter");
 const Users = require("./users-models/users-model.js");
+const session = require("express-session");
 
 const server = express();
 
+const sessionConfig = {
+  name: "cookie",
+  secret: "keep it secret, keep it safe!",
+  cookie: {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 2,
+    secure: false
+  },
+  resave: false,
+  saveUninitialized: true
+};
+
+server.use(session(sessionConfig));
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
@@ -27,24 +41,30 @@ server.use(
 );
 
 function authenticationMiddleware(req, res, next) {
-  const { username, password } = req.headers;
-
-  if (username && password) {
-    Users.findBy({ username })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: `You shall not pass!` });
-        }
-      })
-      .catch(error => {
-        sendUserError(500, error, res);
-      });
+  if (req.session && req.session.username) {
+    next();
   } else {
-    res.status(400).json({ message: "You shall not pass!" });
+    res.status(401).json({ message: `You DEFINITELY shall not pass!` });
   }
+
+  // const { username, password } = req.headers;
+
+  // if (username && password) {
+  //   Users.findBy({ username })
+  //     .first()
+  //     .then(user => {
+  //       if (user && bcrypt.compareSync(password, user.password)) {
+  //         next();
+  //       } else {
+  //         res.status(401).json({ message: `You shall not pass!` });
+  //       }
+  //     })
+  //     .catch(error => {
+  //       sendUserError(500, error, res);
+  //     });
+  // } else {
+  //   res.status(400).json({ message: "You shall not pass!" });
+  // }
 }
 
 module.exports = server;
